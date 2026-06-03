0ff379ad8d1d3e14fbbd3c9f3e469012f64b6e0e
bispomariah-ai <bispo.mariah@escola.pr.gov.br>
2026-06-03 11:30:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const botao = document.getElementById("btnMensagem");
 
+botao.addEventListener("click", () => {
+    alert("O Agrinho 2026 incentiva a inovação, sustentabilidade e educação para um futuro melhor!");
+});
+
+let numero = 0;
+const contador = document.getElementById("contador");
+
+const intervalo = setInterval(() => {
+    numero += 5;
+    contador.textContent = numero;
+
+    if (numero >= 1000) {
+        clearInterval(intervalo);
+    }
+}, 10);
